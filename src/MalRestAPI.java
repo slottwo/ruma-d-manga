1f57af5b585b4d141e31fc26be901e2ea19bbb72
--global <--global>
2023-04-23 17:43:25 -0300

    Outline of MAL API consumption

---
+++ src/MalRestAPI.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class MalRestAPI {
+    public static void main(String[] args) throws Exception {
+        String CLIENT_ID;
+
+        File file = new File("CLIENT-ID");
+        try (Scanner reader = new Scanner(file)) {
+            if (reader.hasNextLine()) {
+                CLIENT_ID = reader.nextLine();
+            } else {
+                throw new Exception("CLIENT-ID file is empty");
+            }
+        }
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.myanimelist.net/v2/manga?q=jojo&limit=3"))
+                .header("X-MAL-CLIENT-ID", CLIENT_ID) // .GET()
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> getResponse = httpClient.send(getRequest, BodyHandlers.ofString());
+
+        System.out.println(getResponse.body());
+    }
+}
