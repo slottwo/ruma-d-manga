be865565f3178dcaab2423829bca76179b644eb5
slottwo <41028091+slottwo@users.noreply.github.com>
2023-06-08 16:57:53 -0300

    Manga

---
+++ src/main/java/br/edu/ufca/rumadmanga/http/response/MangaList.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ufca.rumadmanga.http.response;
+
+import java.net.URI;
+import java.util.List;
+import br.edu.ufca.rumadmanga.manga.Manga;
+
+/**
+ *
+ * @author slottwo
+ */
+public class MangaList {
+
+    private List<Manga> data;
+    private URI previous;
+    private URI next;
+
+}
